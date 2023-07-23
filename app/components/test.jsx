7494af2a41464b35746db808bd8cc227c6c019df
balldev1 <balldev1@hotmail.com>
2023-07-23 13:13:22 +0700

    first commit

---
+++ app/components/test.jsx
@@ -0,0 +1,40 @@
+{/* <div className=' sm: hidden md:block lg-hidden h-screen  bg-gradient-to-b from-orange-400  to-orange-200 flex-col justify-between    '>
+<Link href='/' className='gap-2 focus:bg-gradient-to-b from-orange-600 to-orange-500 focus:shadow-md focus:shadow-slate-500 flex  items-center p-2  ml-2 border-none rounded-l-xl'>
+    <FaUser size={22} />
+    <h2 >เพจและแอดมิน</h2>
+</Link>
+<div className='flex flex-col  mt-20 ml-15 text-white text-sm gap-3 pr-5 '>
+    <Link href='/' className='gap-2 focus:bg-gradient-to-b from-orange-600 to-orange-500 focus:shadow-md focus:shadow-slate-500 flex  items-center p-2   ml-2  border-none rounded-l-xl'>
+        <BsFillChatFill size={25} />
+        <h2>ข้อความ</h2>
+    </Link>
+    <Link href='/' className='gap-2 focus:bg-gradient-to-b from-orange-600 to-orange-500 focus:shadow-md focus:shadow-slate-500 flex  items-center p-2  ml-2 border-none rounded-l-xl'>
+        <FaUser size={22} />
+        <h2 >เพจและแอดมิน</h2>
+    </Link>
+    <Link href='/' className='gap-2 focus:bg-gradient-to-b from-orange-600 to-orange-500 focus:shadow-md focus:shadow-slate-500 flex items-center p-2 ml-2 border-none rounded-l-xl'>
+        <FaShoppingCart size={22} />
+        <h2>คำสั่งซื้อ</h2>
+    </Link>
+    <Link href='/' className='gap-2 focus:bg-gradient-to-b from-orange-600 to-orange-500 focus:shadow-md focus:shadow-slate-500 flex  items-center p-2 ml-2 border-none rounded-l-xl'>
+        <GoHomeFill size={22} />
+        <h2>คลังสินค้า</h2>
+    </Link>
+    <Link href='/' className='gap-2 focus:bg-gradient-to-b from-orange-600 to-orange-500 focus:shadow-md focus:shadow-slate-500 flex  items-center p-2 ml-2 border-none rounded-l-xl'>
+        <AiFillPieChart size={22} />
+        <h2>รายงาน</h2>
+    </Link>
+    <Link href='/' className='gap-2 focus:bg-gradient-to-b from-orange-600 to-orange-500 focus:shadow-md focus:shadow-slate-500 flex items-center p-2 ml-2 border-none rounded-l-xl'>
+        <BiSolidBot size={22} />
+        <h2>บอท</h2>
+    </Link>
+    <Link href='/' className='gap-2 focus:bg-gradient-to-b from-orange-600 to-orange-500 focus:shadow-md focus:shadow-slate-500 flex items-center p-2 ml-2 border-none rounded-l-xl'>
+        <FaShippingFast size={22} />
+        <h2>ขนส่ง</h2>
+    </Link>
+    <Link href='/' className='gap-2 focus:bg-gradient-to-b from-orange-600 to-orange-500 focus:shadow-md focus:shadow-slate-500 flex  items-center p-2 ml-2 border-none rounded-l-xl'>
+        <AiFillSetting size={22} />
+        <h2>ตั้งค่าระบบ</h2>
+    </Link>
+</div>
+</div> */}
